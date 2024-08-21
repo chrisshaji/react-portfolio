b2bf644ba6150e2f9ca2a1af952aba2e0e482b33
Chris Shaji <chrisshaji1234@gmail.com>
2024-08-21 14:28:27 -0500

    cleaned up app.css a little

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,7 @@
 import { Container, Row, Col, } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
-import projImg2 from "../assets/img/project-img2.png";
+import projImg2 from "../assets/img/project-img2.jpg";
 import projImg3 from "../assets/img/project-img3.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
@@ -12,7 +12,7 @@ export const Projects = () => {
   const projects = [
     {
       title: "Rubik's Cube Solver",
-      description: "Fully 3D printed connector a",
+      description: "A machine that solves Rubik's cubes in under 5 seconds",
       imgUrl: projImg1,
     },
     {
