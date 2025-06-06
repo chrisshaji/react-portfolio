d46f91de55ad5100128a2cb6e16acd7344735bba
Chris Shaji <chrisshaji1234@gmail.com>
2025-06-05 20:03:03 -0500

    desc change

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -34,18 +34,19 @@ export const Projects = () => {
       imgUrl: projImg4,
       linkUrl: "https://github.com/chrisshaji/Kitchen-Organizer"
     },
-    {
-      title: "ASME Go Kart",
-      description: "A drivable Go-Kart made from scratch",
-      imgUrl: projImg4,
-      linkUrl: "https://github.com/chrisshaji/ASME-Go-Kart-2024-25"
-    },
-    {
-      title: "Scissor Lift",
-      description: "Class project for MEEN 360",
-      imgUrl: projImg4,
-      linkUrl: "https://github.com/chrisshaji/Scissor-Lift"
-    },
+
+    // {
+    //   title: "ASME Go Kart",
+    //   description: "A drivable Go-Kart made from scratch",
+    //   imgUrl: projImg4,
+    //   linkUrl: "https://github.com/chrisshaji/ASME-Go-Kart-2024-25"
+    // },
+    // {
+    //   title: "Scissor Lift",
+    //   description: "Class project for MEEN 360",
+    //   imgUrl: projImg4,
+    //   linkUrl: "https://github.com/chrisshaji/Scissor-Lift"
+    // },
   ];
 
   return (
