14a867720cb2ad89b01d4550b1a0ebc60d441a66
Chris Shaji <chrisshaji1234@gmail.com>
2026-08-30 15:24:50 -0500

    Update portfolio photo and student status

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,5 @@
 import { Container, Row, Col } from "react-bootstrap";
-import headerImg from "../assets/img/header-img.svg";
+import headerImg from "../assets/img/chris-photo.jpg";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
@@ -14,7 +14,7 @@ export const Banner = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <h1>About Me</h1>
-                <p>My name is Chris Shaji and I am currently a third year Mechanical Engineering student at Texas A&M. My areas of interest include Manufacturing and Design. I take a lot of pride in coming up with new iterations for each design, constantly finding ways to optimize finer details. I seek roles that challenges me to innovate and apply my creativity. In my free time, I enjoy working on personal engineering projects, watching new tv shows, and playing soccer.</p>
+                <p>My name is Chris Shaji and I am currently a Master's student in Mechanical Engineering at Texas A&M. My areas of interest include Manufacturing and Design. I take a lot of pride in coming up with new iterations for each design, constantly finding ways to optimize finer details. I seek roles that challenges me to innovate and apply my creativity. In my free time, I enjoy working on personal engineering projects, watching new tv shows, and playing soccer.</p>
 
               </div>}
             </TrackVisibility>
